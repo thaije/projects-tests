51dc6bf0256d85bdf1cbd2fea8b2dca925a0c18c
Tjalling <tjalling_haije@Outlook.com>
2019-03-31 03:15:10 +0200

    namepsace + rooms example working

---
+++ socket_io_tests/flask-socketio-ns-rooms/server.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, render_template, jsonify
+from flask_socketio import SocketIO, join_room, emit
+from time import sleep
+import numpy as np
+from threading import Lock
+
+
+
+# app = Flask(__name__)
+app = Flask(__name__, template_folder='static/templates')
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+thread = None
+thread_lock = Lock()
+
+
+# send a periodic message to a specific room
+def background_thread(room):
+    while True:
+        socketio.sleep(1)
+        emit('test', "Hey room " + str(room) + "!", room=room)
+        emit('test', "Hey room " + str(room) + "!", room=room)
+
+
+# route for agent, get the ID from the URL
+@app.route('/agent/<id>')
+def index_a(id):
+    return render_template('agent.html', id=id)
+
+
+# route for agent, get the ID from the URL
+@app.route('/human-agent/<id>')
+def index_ha(id):
+    return render_template('human_agent.html', id=id)
+
+
+
+# add client to a room for their specific (human) agent ID
+@socketio.on('join', namespace='/agent')
+@socketio.on('join', namespace='/humanagent')
+def join(message):
+    join_room(message['room'])
+    print("Added client to room:", message["room"])
+
+    emit('test', "Hey room " + message['room'] + "!", room=message['room'])
+    print("sending message to room", message['room'])
+
+# receive pings and send pongs after 1s
+@socketio.on('my_ping', namespace='/agent')
+@socketio.on('my_ping', namespace='/humanagent')
+def ping_pong():
+    socketio.sleep(1)
+    emit('my_pong')
+
+
+@socketio.on('test', namespace="/agent")
+def handle_test_a(message):
+    print('received test from /agent: ' + message)
+
+
+@socketio.on('test', namespace="/humanagent")
+def handle_test_ha(message):
+    print('received test from /humanagent: ' + message)
+
+@socketio.on('my_ping', namespace='/agent')
+@socketio.on('my_ping', namespace='/humanagent')
+@socketio.on('connect')
+def test_connect():
+    print('got a connect')
+
+
+if __name__ == "__main__":
+    print("Starting server")
+    socketio.run(app, port=3000)
---
+++ socket_io_tests/flask-socketio-ns-rooms/static/js/human_agent.js
@@ -0,0 +1,41 @@
+var namespace = "/humanagent";
+
+// make connection with python server via socket
+var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
+
+// get ID of current agent
+var id = document.getElementById('id').innerHTML;
+
+// Connect to server
+socket.on('connect', function() {
+    var room = "/humanagent/" + id;
+
+    // request to be added to room
+    console.log("Requesting to be added to room:", room)
+    socket.emit('join', {room: room});
+});
+
+
+// receive a test update from the python server
+socket.on('test', function(data){
+    console.log("got a message:", data);
+});
+
+
+
+// // send message to Python server every 1 second
+// setInterval(function() {
+//     socket.emit("test", "yo from humanagent GUI");
+//     console.log("Sending test");
+// }, 1000);
+
+document.onkeydown = checkArrowKey;
+function checkArrowKey(e) {
+    e = e || window.event;
+
+    if (e.keyCode < 37 || e.keyCode > 40) {
+        return;
+    }
+
+    socket.emit("test", "Hello from humanagent");
+}
